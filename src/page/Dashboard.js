dad407b20019071197111fc50d18039d01d5b926
dncdrn <missnentin@gmail.com>
2021-06-30 00:36:09 +0800

    added space

--- src/page/Dashboard.js
+++ src/page/Dashboard.js
@@ -54,6 +54,7 @@ export default function Dashboard() {
           />
         }
       >
+        
         <CountryTable
           isTableLoading={isTableLoading}
           selectedCountryKeys={selectedCountryKeys}
